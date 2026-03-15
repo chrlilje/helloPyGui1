58b1c12ed09203d1ff0453439d2487c19531e7b0
Christian Liljedahl <christian@liljedahl.dk>
2026-03-15 19:16:48 +0100

    small updates

--- app.py
+++ app.py
@@ -28,11 +28,14 @@ class App:
 
         # Drain the queue and display the latest data
         latest_data = None
-        try:
-            while not self.data_queue.empty():
-                latest_data = self.data_queue.get_nowait()
-        except queue.Empty:
-            pass 
+
+        while not self.data_queue.empty():
+            try:
+                # get_nowait means: Get data immediately. If nothing is there, it raises queue.Empty exception
+                latest_data = self.data_queue.get_nowait() 
+            except queue.Empty:
+                # Safe way to handle if the queue was emptied somewhere else for some reason
+                break # break from the 'while not self.data_queue.empty()'
 
         if latest_data:
             # keys match what data_io.py sends into the queue
